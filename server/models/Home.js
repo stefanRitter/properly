4e9fbb58e2b99cebdda543732a2656470f2c4d5e
stefanRitter <stefan@stefanritter.com>
2014-04-18 11:00:33 +0100

    crate model

--- server/models/Home.js
+++ server/models/Home.js
@@ -5,14 +5,39 @@ var mongoose = require('mongoose'),
 
 
 homeSchema = mongoose.Schema({
+  type: {type: String, index: true},
+  longRent: {type: Boolean, index: true},
+  shortRent: {type: Boolean, index: true},
+  price: {type: Number, index: true},
+  bedrooms: {type: Number, index: true},
+  bathrooms: {type: Number, index: true},
+    
+  postcode: {type: String},
+  street: {type: String},
+  city: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    default: 'london'
+  },
+  loc: {
+    type: [Number],
+    index: '2dsphere'
+  },
   
+  description: {type: String},
+
+  contactEmail: {type: Boolean},
+  contactPhone: {type: Boolean},
+
+  pictures: [String]
 });
 
 Home = mongoose.model('Home', homeSchema);
 
 // seed homes
 exports.createDefaultHomes = function() {
-  Home.find({}).exec(function(err, collection) {
+  /*Home.find({}).exec(function(err, collection) {
     if (collection.length === 0) {
       
       Home.findOneAndUpdate({}, {},
@@ -20,5 +45,5 @@ exports.createDefaultHomes = function() {
         if (err) { throw err; }
       });
     }
-  });
+  });*/
 };
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
 var User = require('./User');
+var Home = require('./Home');
 
 User.createDefaultUsers();
+Home.createDefaultHomes();
