3f8af39996eee60c88bac1dbb953e0aee81c14d3
stefanRitter <stefan@stefanritter.com>
2014-04-19 18:10:54 +0100

    setup side view

--- app/homes/appHomeSideView.js
+++ app/homes/appHomeSideView.js
@@ -5,11 +5,15 @@ angular.module('app').directive('homeSideView', function (appCachedHomes) {
     restrict: 'C',
     replace: false,
     priority: 1000,
+    templateUrl: '/partials/homes/show',
     controller: ['$scope', '$element', function($scope) {
       $scope.home = {};
 
       $scope.$on('appShowHome', function(e, data) {
         if ($scope.home._id === data._id) { return; }
+        angular.element(document.getElementById('blackout')).addClass('show');
+        angular.element(document.getElementById('homeView')).addClass('show');
+
         $scope.home._id = data._id;
         $scope.home = appCachedHomes.get(data._id);
       });
--- public/js/admin.js
+++ public/js/admin.js
@@ -624,11 +624,15 @@ angular.module('app').factory('appCachedHomes', function (AppHome) {
     restrict: 'C',
     replace: false,
     priority: 1000,
+    templateUrl: '/partials/homes/show',
     controller: ['$scope', '$element', function($scope) {
       $scope.home = {};
 
       $scope.$on('appShowHome', function(e, data) {
         if ($scope.home._id === data._id) { return; }
+        angular.element(document.getElementById('blackout')).addClass('show');
+        angular.element(document.getElementById('homeView')).addClass('show');
+
         $scope.home._id = data._id;
         $scope.home = appCachedHomes.get(data._id);
       });
